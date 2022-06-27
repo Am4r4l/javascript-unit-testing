961972e68160f5727124d9ebc80ab4c052c37c3a
Guilherme Amaral <guilhermeamaral@frwk.com>
2022-06-27 14:09:25 -0300

    projeto inicial

---
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -0,0 +1,31 @@
+import { it, expect } from 'vitest';
+
+import { transformToNumber } from './numbers';
+
+it('should transform a string number to a number of type number', () => {
+    const num = '1';
+
+    const result = transformToNumber(num);
+
+    expect(result).toBeTypeOf('number');
+});
+
+it('should transform a string number to a number of type number', () => {
+    const num = '1';
+
+    const result = transformToNumber(num);
+
+    expect(result).toBe(+num);
+});
+
+
+it('should yield NaN for non-transformable values', () => {
+    const input = 'invalid';
+    const input2 = {}
+
+    const result = transformToNumber(input);
+    const result2 = transformToNumber(input2);
+
+    expect(result).toBeNaN();
+    expect(result2).toBeNaN();
+});
